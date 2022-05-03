789b34d98bd732d3963fcaf78e7270ac44c4d7aa
Arezou Saremian <arezoosaremian71@gmail.com>
2022-05-03 21:05:11 +0430

    Fix scroll pagination.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,56 +1,22 @@
 import React, { Component } from 'react';
 import Vendors from './vendor/Vendors';
 import SearchBox from './shared/SearchBox';
-import Filter from './shared/Filter'
 class Home extends Component {
     state = {
         keyword:'',
-        filters:[
-            {
-                id:1,
-                title:'دارای کوپن',
-                name:'has_coupon',
-                value:false
-            },
-            {
-                id:2,
-                title:'دارای تخفیف',
-                name:'has_discount',
-                value:false
-            }
-
-        ],
-        filter:{}
     }
     search = (keyword) =>{
         this.setState({keyword})
     }
-    filter = (filterName, filterValue) =>{
-        let filter ={
-            name:filterName,
-            value:filterValue
-        }
-        this.setState({filter})
-    }
     render() { 
-        const {filters, keyword, filter} = this.state
+        const {keyword, filter} = this.state
         return (
             <div className="home">
+
                 <SearchBox search={this.search}/>
 
-                <div className="filters">
-                    {filters.map(filter => (
-                        <Filter 
-                            key={filter.id}
-                            title={filter.title}
-                            name={filter.name}
-                            filter={this.filter}/>
-                    ))}
-                </div>
-
-                <Vendors 
-                    keyword={keyword}
-                    filter={filter}/>
+                <Vendors keyword={keyword}/>
+                
             </div>
             );
     }
--- src/components/vendor/Vendors.jsx
+++ src/components/vendor/Vendors.jsx
@@ -1,81 +1,58 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Vendor from './Vendor';
 import './vendors.scss'
-class Vendors extends Component {
-    state = {
-        vendors:[],
-        openShops:{},
-        filteredVendors:[]
-    }
-    
-    componentDidMount(){
-        this.getVendors() 
-    }
-    componentDidUpdate(prevProps){
-        if(prevProps.keyword !== this.props.keyword){
-            this.searchVendors()
-        }
-        if(prevProps.filter.value !== this.props.filter.value || prevProps.filter.name !== this.props.filter.name){
-            this.filterVendors()
-        }
-    }
-    searchVendors(){
-        const filteredVendors = this.state.vendors.filter(vendor => vendor.data.title.includes(this.props.keyword)) 
-        this.setState({filteredVendors})
-    }
-    filterVendors(){
-        let filteredVendors;
-        if(this.props.filter.value){
-            const prop=String(this.props.filter.name)
-            filteredVendors = this.state.vendors.filter(vendor => vendor.data[prop] === true)
-            console.log(filteredVendors)
-            
-        } else{
-            filteredVendors=this.state.vendors
-        }
-        
-        this.setState({filteredVendors})
-    }
-    async getVendors(){
+const PAGE_NUMBER = 10
+const Vendors = ({keyword}) => {
+    const [page, setPage] = useState(PAGE_NUMBER )
+    const [vendors, setVendors] = useState([])
+    const [openShops, setOpenShops]= useState({})
+
+    useEffect(() =>{
         const params = {
             page: 0,
-            page_size: 10,
+            page_size: page,
             lat: 35.754,
             long: 51.328 
         }
-        try{
-            const {data} = await axios.get('https://snappfood.ir/mobile/v3/restaurant/vendors-list', {params})
-            if(data){
-                const vendors = data.data.finalResult.filter(d=> d.type === 'VENDOR')
-                const openShops = (data.data.finalResult.filter(d=> d.type === 'TEXT')).find(d=>d.type ==='TEXT')
-                this.setState({vendors, openShops, filteredVendors : vendors})
-            }
-        }
-        catch(e){
-            console.log(e)
-        }
-    }
-    render() { 
-        const {filteredVendors, openShops} = this.state
-        return (
-            <div className="vendors">
+            axios.get('https://snappfood.ir/mobile/v3/restaurant/vendors-list', {params})
+            .then(res => {
+                if(res.data) {
+                    let vendorsList = res.data.data.finalResult.filter(d=> d.type === 'VENDOR')
+                    vendorsList = vendorsList.filter(vendor => vendor.data.title.includes(keyword)) 
+                    const openShopsCount= (res.data.data.finalResult.filter(d=> d.type === 'TEXT')).find(d=>d.type ==='TEXT')
+                    setVendors(vendorsList)
+                    setOpenShops(openShopsCount)
+                }
+            }).catch(e=> console.log(e))
+    }, [page, keyword])
 
-                <p className="vendors__count">
-                    {openShops.data}
-                </p>
-                
-                <div className="vendors__items">
-                    {filteredVendors.map((vendor, index) => (
-                        <Vendor 
-                            key={index}
-                            vendor={vendor}
-                        />
-                    ))}
-                </div>
-                
-            </div>);
+    const scrollToEnd = function(){
+        setPage(page+1)
     }
+    window.onscroll = function(){
+        if( window.innerHeight + document.documentElement.scrollTop ===  document.documentElement.offsetHeight){
+            scrollToEnd()
+        }
+    }
+    return (  
+        <div className="vendors">
+
+            <p className="vendors__count">
+                {openShops.data}
+            </p>
+
+            <div className="vendors__items">
+                {vendors.map((vendor, index) => (
+                    <Vendor 
+                        key={index}
+                        vendor={vendor}
+                    />
+                ))}
+            </div>
+    
+        </div>
+    );
 }
  
 export default Vendors;
